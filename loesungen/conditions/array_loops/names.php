559d278b2809c2461f9a8ff847acd4c46f7c7fc4
mmcbbq <m.martinez@bbq.de>
2025-09-26 08:11:58 +0200

    add array_loops

---
+++ loesungen/conditions/array_loops/names.php
@@ -0,0 +1,127 @@
+<?php
+$users = [
+    [
+        "lastname" => "Abo Shameh",
+        "firstname" => "Ahmad",
+        "domain" => "www.ahmad.bbq.zz.",
+        "ip" => "10.101.105.100"
+    ],
+    [
+        "lastname" => "Bafaiz",
+        "firstname" => "Zamir",
+        "domain" => "www.zamir.bbq.zz.",
+        "ip" => "10.101.105.105"
+    ],
+    [
+        "lastname" => "Bayar",
+        "firstname" => "Gülden",
+        "domain" => "www.guelden.bbq.zz.",
+        "ip" => "10.101.105.110"
+    ],
+    [
+        "lastname" => "Bock",
+        "firstname" => "Christian",
+        "domain" => "www.christian.bbq.zz.",
+        "ip" => "10.101.105.115"
+    ],
+    [
+        "lastname" => "Borchhardt",
+        "firstname" => "Gero",
+        "domain" => "www.gero.bbq.zz.",
+        "ip" => "10.101.105.120"
+    ],
+    [
+        "lastname" => "Bradley",
+        "firstname" => "Sian",
+        "domain" => "www.sian.bbq.zz.",
+        "ip" => "10.101.105.125"
+    ],
+    [
+        "lastname" => "Davaasuren",
+        "firstname" => "Sod-Erdene",
+        "domain" => "www.sod-erdene.bbq.zz.",
+        "ip" => "10.101.105.130"
+    ],
+    [
+        "lastname" => "Farmakis",
+        "firstname" => "Alexander",
+        "domain" => "www.alexander.bbq.zz.",
+        "ip" => "10.101.105.135"
+    ],
+    [
+        "lastname" => "Iordachescu",
+        "firstname" => "Stefan-Claudiu",
+        "domain" => "www.stefan-claudiu.bbq.zz.",
+        "ip" => "10.101.105.140"
+    ],
+    [
+        "lastname" => "Kimura",
+        "firstname" => "Ayano",
+        "domain" => "www.ayano.bbq.zz.",
+        "ip" => "10.101.105.145"
+    ],
+    [
+        "lastname" => "Kocatürk",
+        "firstname" => "Fikret",
+        "domain" => "www.fikret.bbq.zz.",
+        "ip" => "10.101.105.150"
+    ],
+    [
+        "lastname" => "Ludwig",
+        "firstname" => "Kilian",
+        "domain" => "www.kilian.bbq.zz.",
+        "ip" => "10.101.105.155"
+    ],
+    [
+        "lastname" => "Magtoofi",
+        "firstname" => "Khaled",
+        "domain" => "www.khaled.bbq.zz.",
+        "ip" => "10.101.105.160"
+    ],
+    [
+        "lastname" => "Müller",
+        "firstname" => "Moritz",
+        "domain" => "www.moritz.bbq.zz.",
+        "ip" => "10.101.105.165"
+    ],
+    [
+        "lastname" => "Bowinkelmann",
+        "firstname" => "Patrick",
+        "domain" => "www.patrick.bbq.zz.",
+        "ip" => "10.101.105.170"
+    ],
+    [
+        "lastname" => "Petruschke",
+        "firstname" => "Dennis",
+        "domain" => "www.dennis.bbq.zz.",
+        "ip" => "10.101.105.175"
+    ],
+    [
+        "lastname" => "Shezleen",
+        "firstname" => "Ahmed",
+        "domain" => "www.ahmed.bbq.zz.",
+        "ip" => "10.101.105.180"
+    ],
+    [
+        "lastname" => "Ajvazi",
+        "firstname" => "Vullnet",
+        "domain" => "www.vullnet.bbq.zz.",
+        "ip" => "10.101.105.185"
+    ],
+    [
+        "lastname" => "Hagedorn",
+        "firstname" => "Bastian",
+        "domain" => "www.bastian.bbq.zz.",
+        "ip" => "10.101.105.190"
+    ],
+    [
+        "lastname" => "Purtov",
+        "firstname" => "Server",
+        "domain" => "www.server.bbq.zz.",
+        "ip" => "10.101.105.195"
+    ]
+];
+
+
+
+
